2adad13e05b7c7bf960dde7518ae2dd24cc355a7
Luke Gorman <lukegorman97@gmail.com>
2017-12-24 21:27:19 +0000

    Add URL parameter for lodges and catch if value is invalid

--- js/lodges.js
+++ js/lodges.js
@@ -1,15 +1,15 @@
 const url = 'js/lodges.json';  
 
 function selectLodge(name) {
-  fetch(url).then(r => r.json())
+  fetch(url).then(response => response.json())
   .then(
     response => response.forEach(function(lodge) {
       if (lodge.name == name) {
-
         // Iterate through lodge features and output onto page
         lodge.features.forEach(function(feature) {
           let featuresParentDiv = document.getElementsByClassName('lodge-features')[0];
 
+          // Create .feature div with .feature-item and .feature-description inside
           featureDiv = document.createElement('div');
           featureDiv.className = 'feature';
 
@@ -24,10 +24,11 @@ function selectLodge(name) {
           featureDiv.appendChild(featureItem);
           featureDiv.appendChild(featureDesc);
 
+          // Append the new .feature div to the parent container
           featuresParentDiv.appendChild(featureDiv);
         });
         
-        // Set the page elements to show lodge details
+        // Set the page elements to show lodge details from JSON file
         let lodgeNameVal = document.getElementsByClassName('lodge-name')[0];
         let lodgeDescriptionVal = document.getElementsByClassName('lodge-description')[0];
         let sectionLodgePhoto = document.getElementById("lodge-photo");
@@ -41,5 +42,20 @@ function selectLodge(name) {
   .catch(e => console.log(e))
 }
 
-// Select first lodge if none specified in URL
-selectLodge('Kingfisher');
+// Get lodge specified in URL parameter
+var base_url = new URL(window.location.href);
+var lodgeParam = base_url.searchParams.get("l");
+
+// Select the lodge from JSON if URL param is valid, else default to 'Kingfisher'
+let lodges = [
+  'Kingfisher',
+  'Badgers Retreat',
+  'Nuthatch',
+  'Dormouse'
+];
+
+if (lodges.includes(lodgeParam)) {
+  selectLodge(lodgeParam);
+} else {
+  selectLodge('Kingfisher');
+}
