3e77c91f5b005d63bfea0707d9566741e2d777b4
Luke Gorman <lukegorman97@gmail.com>
2017-12-25 11:49:37 +0000

    Add smooth scrolling button effects on index

--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
-var sectionLodgePhoto = document.getElementById("lodge-photo");
-sectionLodgePhoto.style.backgroundImage = "url('https://c1.staticflickr.com/5/4444/37944893541_d3bd54752d_h.jpg')";
+// JavaScript to enable smooth scrolling on button clicks
+
+let learnMoreBtn = document.getElementById('learnMoreBtn');
+let lakesInfoBtn = document.getElementById('lakesInfoBtn');
+
+let introDiv = document.getElementById('intro');
+let lakesDiv = document.getElementById('lakes');
+
+learnMoreBtn.addEventListener('click', function() {
+  introDiv.scrollIntoView({behavior: 'smooth'});
+});
+
+lakesInfoBtn.addEventListener('click', function() {
+  lakesDiv.scrollIntoView({behavior: 'smooth'});
+});
